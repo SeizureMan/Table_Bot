56495f74953ddef92caf3ac5067aec28d9f28ed2
Julius Seizure <billymark6@gmail.com>
2014-01-19 11:44:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ import time
 user_agent = ("The Table Bot")
 
 # This is our reddit object that uses the user agent as a user agent :O
+#user agents are the reddit bot
 reddit = praw.Reddit(user_agent = user_agent)
+#this tells reddit what bot we are
